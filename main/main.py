8cf44b73437b58f82d9ca7a5a5ffe9dfcda8aa08
Jackson Hoffart <jackson.hoffart@gmail.com>
2017-11-07 22:31:30 -0500

    digit recognition tutorial from scikit-learn

--- main/main.py
+++ main/main.py
@@ -1,1 +1,21 @@
+# Authors:
+# Bryce Walter Parker <brycewalterparker at gmail dot com>
+# Jackson Hoffart <jackson dot hoffart at gmail dot com>
+
+# License: GPL-3.0
+
+# include this when deploying!
+# ! pip freeze > requirements.txt
+
+''' import some cheeky modules '''
+# standard modules
 import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+%matplotlib inline
+
+''' sci-kit learn handwritten digit recognition tutorial'''
+# Import datasets, classifiers and performance metrics
+from sklearn import datasets, svm, metrics
